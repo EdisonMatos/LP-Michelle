3744ec97d4bf8cfefcf7a72d73e49242a46470d6
Edison Matos <edison.matos@live.com>
2025-01-22 11:46:25 -0300

    Initial commit

---
+++ src/content/contentLp01.jsx
@@ -0,0 +1,345 @@
+import bgImg from "../assets/imgs/hero/bgHeroLp01.webp";
+import logo from "../assets/imgs/logo/logoLp.png";
+import { infos } from "./content";
+
+const currentYear = new Date().getFullYear();
+
+const contentLp01 = {
+  infos: {
+    title: "Dra. Michelle Florentino - Garanta seu BPC/LOAS",
+    description:
+      "Precisa de ajuda para garantir seu BPC/LOAS? A Dra. Michelle Florentino, especialista em direito previdenciário, oferece orientação personalizada para assegurar seus direitos. Entre em contato e conquiste o benefício que você merece.",
+  },
+  hero: {
+    bgImg: bgImg,
+    logo: {
+      logoImg: logo,
+      alt: "Logomarca",
+    },
+    textArea: {
+      title: (
+        <h1>
+          <span className="text-primary"> Qualidade de vida </span> ao seu
+          alcance com o<span className="text-primary"> BPC/LOAS! </span>
+        </h1>
+      ),
+      subtitle:
+        "Descubra como acessar o BPC/LOAS e proporcionar bem-estar para quem mais precisa. Estamos aqui para ajudar você e sua família.",
+      buttonLabel: "Quero solicitar o BPC/LOAS",
+      ctaButtonAriaLabel:
+        "Botão para chamada de ação para contato pelo whatsapp",
+    },
+  },
+  features: {
+    sectionHeader: {
+      miniTag: "SEU DIREITO AO BPC/LOAS",
+      title: "Quem pode se qualificar?",
+    },
+    cards: {
+      card1: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-hand-coins"
+          >
+            <path d="M11 15h2a2 2 0 1 0 0-4h-3c-.6 0-1.1.2-1.4.6L3 17" />
+            <path d="m7 21 1.6-1.4c.3-.4.8-.6 1.4-.6h4c1.1 0 2.1-.4 2.8-1.2l4.6-4.4a2 2 0 0 0-2.75-2.91l-4.2 3.9" />
+            <path d="m2 16 6 6" />
+            <circle cx="16" cy="9" r="2.9" />
+            <circle cx="6" cy="5" r="3" />
+          </svg>
+        ),
+        title: "Idosos em situação de risco",
+        description:
+          "Se você ou um ente querido acima de 65 anos está em situação de vulnerabilidade social e não tem acesso à aposentadoria, o BPC/LOAS pode ser a solução para assegurar seus direitos.",
+      },
+      card2: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-car"
+          >
+            <path d="M19 17h2c.6 0 1-.4 1-1v-3c0-.9-.7-1.7-1.5-1.9C18.7 10.6 16 10 16 10s-1.3-1.4-2.2-2.3c-.5-.4-1.1-.7-1.8-.7H5c-.6 0-1.1.4-1.4.9l-1.4 2.9A3.7 3.7 0 0 0 2 12v4c0 .6.4 1 1 1h2" />
+            <circle cx="7" cy="17" r="2" />
+            <path d="M9 17h6" />
+            <circle cx="17" cy="17" r="2" />
+          </svg>
+        ),
+        title: "Pessoas com deficiência ou condições graves de saúde",
+        description:
+          "Se você ou um membro da sua família tem uma deficiência ou uma doença grave, pode ter direito ao BPC. Realize uma avaliação e veja como garantir esse benefício.",
+      },
+      card3: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-triangle-alert"
+          >
+            <path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3" />
+            <path d="M12 9v4" />
+            <path d="M12 17h.01" />
+          </svg>
+        ),
+        title: "Crianças no espectro autista",
+        description:
+          "Famílias com crianças autistas podem ter acesso ao BPC, ajudando a cobrir despesas com terapias e cuidados médicos fundamentais. Saiba mais sobre como solicitar.",
+      },
+      card4: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-file-check"
+          >
+            <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" />
+            <path d="M14 2v4a2 2 0 0 0 2 2h4" />
+            <path d="m9 15 2 2 4-4" />
+          </svg>
+        ),
+        title: "Benefício recusado ou suspenso",
+        description:
+          "Se o seu benefício foi indevidamente recusado ou suspenso, buscamos reverter a decisão e garantir que seus direitos ao BPC sejam respeitados.",
+      },
+    },
+  },
+  about: {
+    bgImg: bgImg,
+    logo: {
+      logoImg: logo,
+      alt: "Logomarca",
+    },
+    sectionHeader: {
+      miniTag: "EXCELÊNCIA EM DEFESA PREVIDENCIÁRIO",
+      title: "Dedicação e eficiência",
+    },
+    paragraph: (
+      <p>
+        Compreendo que, em questões previdenciárias, a agilidade é essencial
+        para nossos clientes. Por isso, atuo de maneira rápida e estratégica,
+        sempre mantendo a excelência na qualidade. Meu compromisso é garantir
+        que seus direitos sejam protegidos com eficiência e determinação. <br />
+        <br /> Independentemente da sua necessidade previdenciária, estou aqui
+        para oferecer a melhor solução jurídica. Entre em contato com Matthäus
+        Advocacia e descubra como podemos ser fundamentais na defesa dos seus
+        direitos.
+      </p>
+    ),
+    buttonLabel: "Quero falar com a Dra. Michelle",
+    ctaButtonAriaLabel: "Botão para chamada de ação para contato pelo whatsapp",
+  },
+  whyUs: {
+    sectionHeader: {
+      miniTag: "GARANTA SEU BPC/LOAS CONOSCO",
+      title: "Por que confiar em nós para defender seus direitos?",
+    },
+    cards: {
+      card1: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-scale"
+          >
+            <path d="m16 16 3-8 3 8c-.87.65-1.92 1-3 1s-2.13-.35-3-1Z" />
+            <path d="m2 16 3-8 3 8c-.87.65-1.92 1-3 1s-2.13-.35-3-1Z" />
+            <path d="M7 21h10" />
+            <path d="M12 3v18" />
+            <path d="M3 7h2c2 0 5-1 7-2 2 1 5 2 7 2h2" />
+          </svg>
+        ),
+        title: "Estratégia sob medida para seu BPC/LOAS",
+        description:
+          "Examinamos seu pedido de BPC/LOAS com atenção aos detalhes. Juntos, criaremos uma estratégia sob medida para garantir que você obtenha o benefício que merece. Conte com nosso apoio em todas as etapas do processo!",
+      },
+      card2: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-file-chart-column-increasing"
+          >
+            <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" />
+            <path d="M14 2v4a2 2 0 0 0 2 2h4" />
+            <path d="M8 18v-2" />
+            <path d="M12 18v-4" />
+            <path d="M16 18v-6" />
+          </svg>
+        ),
+        title: "Plano de ação personalizado",
+        description:
+          "Desenvolvemos um plano de ação exclusivo para seu caso, focando em uma solução rápida e eficiente para o seu BPC/LOAS. Cada detalhe é pensado para atender suas necessidades. Estamos aqui para transformar sua situação em uma conquista.",
+      },
+      card3: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-history"
+          >
+            <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8" />
+            <path d="M3 3v5h5" />
+            <path d="M12 7v5l4 2" />
+          </svg>
+        ),
+        title: "Foco total na sua aprovação",
+        description:
+          "Nosso compromisso é garantir que seu pedido de BPC/LOAS seja aprovado. Defendemos seus direitos com dedicação em todas as etapas, cuidando de cada detalhe para alcançar o resultado que você merece.",
+      },
+      card4: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-shield-check"
+          >
+            <path d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z" />
+            <path d="m9 12 2 2 4-4" />
+          </svg>
+        ),
+        title: "Confiança respaldada por experiência",
+        description:
+          "Com vasta experiência em pedidos de BPC/LOAS, cuidamos do seu processo com segurança e eficiência. Estaremos ao seu lado, garantindo que seus direitos sejam respeitados e que você receba o benefício a que tem direito. Confie em nós para fazer a diferença!",
+      },
+    },
+  },
+  cta: {
+    bgImg: bgImg,
+    sectionHeader: {
+      miniTag: "CHEGOU A HORA",
+      title: "Conquiste seu BPC/LOAS hoje mesmo!",
+      subtitle: `Com a Dra. Michelle Florentino, você conta com uma especialista em direito previdenciário para orientar e assegurar seu direito ao BPC/LOAS. Não deixe que dificuldades financeiras ou burocráticas impeçam você de conquistar o benefício que merece.`,
+    },
+    buttonLabel: "Quero solicitar o BPC/LOAS",
+    ctaButtonAriaLabel: "Botão para chamada de ação para contato pelo whatsapp",
+  },
+  contact: {
+    card1: {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="28"
+          height="28"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-instagram"
+        >
+          <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+          <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
+          <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
+        </svg>
+      ),
+      title: "Instagram",
+      description: `@${infos.instagramProfile}`,
+    },
+    card2: {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="28"
+          height="28"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-mail"
+        >
+          <rect width="20" height="16" x="2" y="4" rx="2" />
+          <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+        </svg>
+      ),
+      title: "Email",
+      description: `${infos.email}@${infos.domain}`,
+    },
+    card3: {
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="currentColor"
+          className="bi bi-whatsapp"
+          viewBox="0 0 16 16"
+        >
+          <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
+        </svg>
+      ),
+      title: "Whatsapp",
+      description: `(${infos.phone.ddd}) ${infos.phone.firstPart}-${infos.phone.secondPart}`,
+    },
+  },
+  footer: {
+    copyrightLine: `© ${currentYear} ${infos.name}. Todos os direitos reservados.`,
+    disclaimer: `Este site não é um produto Meta Platforms, Inc., Google LLC, tampouco oferece serviços públicos oficiais. ${infos.name} oferece serviços jurídicos privativos de advogado, de acordo com a legislação vigente e o Código de Ética e Disciplina da Ordem dos Advogados do Brasil.`,
+  },
+  links: {
+    instagram: `https://www.instagram.com/${infos.instagramProfile}/`,
+    ctaWhatsapp: `https://wa.me/+55${infos.phone.ddd}${infos.phone.firstPart}${infos.phone.secondPart}?text=${infos.whatsappDefaultMessage}`,
+  },
+};
+
+export default contentLp01;
